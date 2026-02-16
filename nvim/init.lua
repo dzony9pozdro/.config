c3b8953432553093799f4c2d1660ad639190f858
dzony9pozdro <jan@tomaka.it>
2026-02-16 20:17:47 +0100

    setup nvim lsp, starting work on cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,25 +1,14 @@
+require("config.lazy")
+require('mini.statusline').setup()
+require("mason").setup()
+require('lsp.lsp')
+require('config.cmp')
 
--- Visual indent stay in selection
-vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true })
-vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })
+vim.lsp.enable('basedpyright')
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+vim.o.relativenumber = true
 
-local o = vim.o        -- global options
-local bo = vim.bo      -- buffer-local options
-local wo = vim.wo      -- window-local options
-
--- Tabs
-o.expandtab = false     -- use actual tabs, not spaces
-o.tabstop = 4           -- a tab character is 4 spaces wide
-o.shiftwidth = 4        -- indentation uses 4 spaces
-o.softtabstop = 4       -- how many spaces a Tab inserts in insert mode
-o.smarttab = true       -- Tab respects shiftwidth at start of line
-
--- Optional: apply buffer-local as well (useful for new buffers)
-bo.expandtab = false
-bo.tabstop = 4
-bo.shiftwidth = 4
-bo.softtabstop = 4
-
-vim.o.cursorline = true;
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#FFFFFF" })
-vim.o.relativenumber = true;
---
+++ nvim/lua/lsp/lsp.lua
@@ -0,0 +1,39 @@
+require("mason").setup()
+
+require("mason-lspconfig").setup({
+    ensure_installed = { "lua_ls", "basedpyright" },
+})
+
+vim.lsp.config("lua_ls", {
+    cmd = { "lua-language-server" },
+    filetypes = { "lua" },
+    root_dir = function(bufnr, on_dir)
+        on_dir(vim.fs.root(bufnr, { ".git", ".luarc.json" }) or vim.fn.expand("%:p:h"))
+    end,
+    settings = {
+        Lua = {
+            runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
+            diagnostics = { globals = { "vim" } },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+            },
+            telemetry = { enable = false },
+        },
+    },
+})
+
+vim.lsp.enable("lua_ls")
+
+vim.lsp.config("basedpyright", {
+    cmd = { "basedpyright-langserver", "--stdio" },
+    filetypes = { "python" },
+    root_dir = function(bufnr, on_dir)
+        on_dir(
+            vim.fs.root(bufnr, { ".git", "pyproject.toml", "setup.py", "requirements.txt" }) 
+            or vim.fn.expand("%:p:h")
+        )
+    end,
+})
+
+vim.lsp.enable("basedpyright")
